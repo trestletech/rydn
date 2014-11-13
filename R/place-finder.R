83ca33bf147ad92ed75e519c6298a96057e9bd93
trestletech <jeff.allen@trestletechnology.net>
2014-11-12 22:45:16 -0600

    Cleaned up find place function.

--- R/place-finder.R
+++ R/place-finder.R
@@ -1,24 +1,44 @@
-
-httr::GET("https://yboss.yahooapis.com/geo/placefinder?location=170%20South%20Market%20St.%2C%20San%20Jose%2C%20CA&flags=J")
-
-url <- "https://yboss.yahooapis.com/geo/placefinder"
-
-
-oa <- httr::oauth_app("YDN", key, secret)
-
-urlq <- paste0(url, "?location=701+first+avenue+sunnnyvale&flags=J")
-
-sig <- httr::oauth_signature(urlq, app=oa)
-
-oaqstr <- ""
-for (i in 1:length(sig)) {
-  val <- sig[[i]]
-  if (is.character(val)){
-    #val <- URLencode(val, TRUE) 
+#' Geolocate an address
+#' 
+#' Lookup the latitude and longitude of a location using the place's
+#' name or address.
+#' @param location The location name or address you want to lookup
+#' @param name The name of a point of interest to lookup
+#' @param key The Yahoo! Developer Network BOSS application key
+#' @param secret The Yahoo! Developer Network BOSS application secret
+find_place <- function(location,
+                       name,
+                       key=getOption("RYDN_KEY"),
+                       secret=getOption("RYDN_SECRET")){
+  if (!missing(location) && !missing(name)){
+    stop("Cannot provide both location and name parameter.")
   }
-  oaqstr <- paste0(oaqstr, "&", names(sig)[i], "=", val)
+  
+  if (missing(location) && missing(name)){
+    stop("Must provide either location or name parameter.")
+  }
+  
+  if (is.null(key) || is.null(secret)){
+    stop("key and secret must both be provided")
+  } 
+  
+  url <- "https://yboss.yahooapis.com/geo/placefinder"
+  oa <- httr::oauth_app("YDN", key, secret)
+  
+  urlq <- paste0(url, "?flags=J&location=", URLencode(location))
+  
+  sig <- httr::oauth_signature(urlq, app=oa)
+  
+  oaqstr <- ""
+  for (i in 1:length(sig)) {
+    val <- sig[[i]]
+    oaqstr <- paste0(oaqstr, "&", names(sig)[i], "=", val)
+  }
+  urlqa <- paste0(urlq, oaqstr)
+  
+  q <- httr::GET(urlqa)
+  stop_for_status(q)
+  
+  res <- content(q)$bossresponse$placefinder
+  do.call(rbind.data.frame, res$results)
 }
-urlqa <- paste0(urlq, oaqstr)
-
-q <- httr::GET(urlqa)
-
